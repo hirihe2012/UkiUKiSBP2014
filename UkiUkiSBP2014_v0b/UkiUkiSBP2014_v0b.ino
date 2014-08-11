f0542fe8f6432f5a1a686b1458a538f996b768a7
hirihe2012 <iriehiroki@iriekenkyuushitsu-no-MacBook-Air.local>
2014-08-11 11:05:16 +0900

    soundsをプラスした
    soundsをプラスした
    d12につながった圧電スピーカから音をだして、経過時間を知らせる。

--- UkiUkiSBP2014_v0b/UkiUkiSBP2014_v0b.ino
+++ UkiUkiSBP2014_v0b/UkiUkiSBP2014_v0b.ino
@@ -2,7 +2,8 @@
 //The version is 0.1b  
 // by Mujirushi Seisakusyo IRIE Ltd. (2014July10)
 
-static const long BurstTmie_ms = 300000;  //5min x 60 x 1000 = 300000
+static const int BurstTime_min = 1;   //5min
+long BurstTmie_ms;  // = 300000;  burstTime_min x 60 x 1000 ms
 
 //Controlling a servo position using a potentiometer (variable resistor) 
 // by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
@@ -27,9 +28,43 @@ int mode_sw = 4; // mode SW connected to digital pin 4
 unsigned long prev_time = 0;
 unsigned long chg_time = 200;
 
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+  
+void sounds(){
+ for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(12, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(12);
+  }
+  
+}
+
+
 void Balloon_Burster(int times)
 {
   int i;
+  
+  sounds();
+  
+//  tone(12,440,300);
   for(i=0;i<times;i++)
   {
     val2 = analogRead(potpin2);
@@ -47,14 +82,34 @@ void Balloon_Burster(int times)
  
 void LEDchika(void)
 {
+  static int i;
+  int ftone;
+  
    if(millis() - prev_time > chg_time){
       prev_time = millis();
       if(digitalRead(led_out) == LOW)
         digitalWrite(led_out,HIGH);
       else
         digitalWrite(led_out,LOW);
+        i++;
+        if(i>10){i=0; 
+       ftone = (int)((BurstTmie_ms-(millis() - StartTime))/1000 );
+         tone(12,880+ftone,100);delay(100); tone(12,880,100);}
     }
 }
+
+void Startsound(void)
+{
+  int i;
+  
+ for(i=0;i<BurstTime_min;i++)
+ {
+ tone(12,440,200);
+ delay(300);
+ }
+ 
+}
+
 
 void setup() 
 { 
@@ -63,10 +118,15 @@ void setup()
   
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   
- if(digitalRead(mode_sw) == _ON) //モードスイッチがオンならば、
-     Balloon_Burster(5);  //最初の５回は試し動作を行う。
+  BurstTmie_ms= (long)BurstTime_min * 60 *1000;
+  
+ //if(digitalRead(mode_sw) == _ON) //モードスイッチがオンならば、
+ //    Balloon_Burster(5);  //最初の５回は試し動作を行う。
      
  StartTime = millis();
+ Startsound();
+ 
+ 
 } 
  
 void loop() 
@@ -78,5 +138,6 @@ void loop()
   }
   
   Balloon_Burster(50);  // 50回動作させる
-  
+  sounds();
+  while(1);
 }
